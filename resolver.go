b2b58eae9b3df15ef6c589e54c0268ea6c56b765
Raion314 <raion314@outlook.com>
2019-03-07 21:33:50 -0800

    add mutation, resolver, and query logic

--- resolver.go
+++ resolver.go
@@ -2,16 +2,50 @@ package gographi
 
 import (
 	"context"
-) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+	"fmt"
+	"math/rand"
+)
 
-type Resolver struct{}
+// Resolver struct
+type Resolver struct {
+	todos []Todo
+}
 
+// Mutation comment
+func (r *Resolver) Mutation() MutationResolver {
+	return &mutationResolver{r}
+}
+
+// Query comment
 func (r *Resolver) Query() QueryResolver {
 	return &queryResolver{r}
+}
+
+// Todo comment
+func (r *Resolver) Todo() TodoResolver {
+	return &todoResolver{r}
+}
+
+type mutationResolver struct{ *Resolver }
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input NewTodo) (*Todo, error) {
+	todo := &Todo{
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		UserID: input.UserID,
+	}
+	r.todos = append(r.todos, *todo)
+	return todo, nil
 }
 
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]Todo, error) {
-	panic("not implemented")
+	return r.todos, nil
+}
+
+type todoResolver struct{ *Resolver }
+
+func (r *todoResolver) User(ctx context.Context, obj *Todo) (*User, error) {
+	return &User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
 }
